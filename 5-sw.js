5d2664a86869dba2ec7aa40dcc91331bef0e0b9a
by5160887-oss <by5160887@gmail.com>
2026-09-17 14:16:01 +0300

    Build Hebrew schedule PWA MVP

---
+++ 5-sw.js
@@ -0,0 +1,4 @@
+const CACHE='luaz-v1'; const ASSETS=['/','/index.html','/manifest.webmanifest','/icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return r}).catch(()=>caches.match(e.request))) });
